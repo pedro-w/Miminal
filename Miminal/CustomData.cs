1577799271eb213496be7b87486dcb5743790bfe
Peter Hull <peterhull90@gmail.com>
2025-09-08 09:20:52 +0100

    Add project files.

---
+++ Miminal/CustomData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miminal
+{
+    public class CustomData()
+    {
+        public string One { get; set; } = "";
+        public string Two
+        {
+            get; set;
+        } = "";
+    }
+}
